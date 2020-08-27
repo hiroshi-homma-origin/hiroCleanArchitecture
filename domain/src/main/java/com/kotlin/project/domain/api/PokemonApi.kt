34cfea029ae834da5b8a6bc5e7e0d372182ddb2a
hiroshi-homma <homma-hiroshi@andfactpry.co.jp>
2020-08-27 23:55:51 +0900

    API adjustment

--- domain/src/main/java/com/kotlin/project/domain/api/PokemonApi.kt
+++ domain/src/main/java/com/kotlin/project/domain/api/PokemonApi.kt
@@ -4,6 +4,6 @@ import com.kotlin.project.data.models.original.response.PokeListResponse
 import retrofit2.http.GET
 
 internal interface PokemonApi {
-    @GET("pokelist/pokedex.json")
+    @GET("pokelist/pokedex0.json")
     suspend fun getPokemonList1(): List<PokeListResponse>
 }
