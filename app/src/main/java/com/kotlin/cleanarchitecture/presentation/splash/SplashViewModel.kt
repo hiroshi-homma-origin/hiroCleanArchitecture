9aca2f928111dc0c73e0b63dec3a20144ea293a0
hiroshi-homma <homma-hiroshi@andfactpry.co.jp>
2020-08-29 03:33:38 +0900

    adjustment2

--- app/src/main/java/com/kotlin/cleanarchitecture/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/com/kotlin/cleanarchitecture/presentation/splash/SplashViewModel.kt
@@ -15,7 +15,6 @@ class SplashViewModel @ViewModelInject constructor(
     @PokemonListUseCase private val useCase: GetPokemonListUseCase
 ) : ViewModel() {
     fun fetchData() {
-        number = 44
         viewModelScope.launch(Dispatchers.Default) {
             Timber.d("check_data:${number}")
             pokeListLiveData.postValue(useCase.getList("pokedex${number}.json"))
